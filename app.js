36aaa058be87700b0a19e3e5fed9b5fff9a55a70
moizrehman-creator <raeesmueez1@gmail.com>
2025-12-13 18:54:50 +0500

    Update Firebase configuration with new credentials

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 ///Edit Under This only
 
     var firebaseConfig = {
-    apiKey: "API KEY",
-    authDomain: "AUTH DOMAIN",
-    databaseURL: "DATABSE URL",
-    projectId: "PROJECT ID",
-    storageBucket: "BUCKET URL",
-    messagingSenderId: "SENDER ID",
-    appId: "APP ID"
-  }; 
+    apiKey: "AIzaSyB_PzgWyCPTiOk-NGNqCME_Ma1wq7oZUHQ",
+  authDomain: "instagram-b992c.firebaseapp.com",
+  databaseURL: "https://instagram-b992c-default-rtdb.firebaseio.com",
+  projectId: "instagram-b992c",
+  storageBucket: "instagram-b992c.firebasestorage.app",
+  messagingSenderId: "338669236196",
+  appId: "1:338669236196:web:3b3f444e949cce7092e1f1",
+  measurementId: "G-3RMSN5VH4N"
+};
  
 ////Do Not Edit Below This
 
