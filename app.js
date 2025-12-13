de94411dd2ffe7ae8d80d01aad9c5b6619861760
moizrehman-creator <raeesmueez1@gmail.com>
2025-12-13 20:09:23 +0500

    Update Firebase configuration and initialization

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
-///Edit Under This only
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-    var firebaseConfig = {
-    apiKey: "AIzaSyB_PzgWyCPTiOk-NGNqCME_Ma1wq7oZUHQ",
-  authDomain: "instagram-b992c.firebaseapp.com",
-  databaseURL: "https://instagram-b992c-default-rtdb.firebaseio.com",
-  projectId: "instagram-b992c",
-  storageBucket: "instagram-b992c.firebasestorage.app",
-  messagingSenderId: "338669236196",
-  appId: "1:338669236196:web:3b3f444e949cce7092e1f1",
-  measurementId: "G-3RMSN5VH4N"
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCrdpa8uqtGN3bsYedY6eWBIrsKJ7rzir8",
+  authDomain: "android-33a8e.firebaseapp.com",
+  projectId: "android-33a8e",
+  storageBucket: "android-33a8e.firebasestorage.app",
+  messagingSenderId: "112901312412",
+  appId: "1:112901312412:web:fad1047f8ea1bb5a958259",
+  measurementId: "G-YDQXNCHFY9"
 };
- 
-////Do Not Edit Below This
 
-firebase.initializeApp(firebaseConfig);
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
