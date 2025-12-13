6949b629522100c235be4813010c788b810c7f9c
moizrehman-creator <raeesmueez1@gmail.com>
2025-12-14 01:09:26 +0500

    Fix Firebase initialization code

--- app.js
+++ app.js
@@ -1,7 +1,3 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
-import { getDatabase } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
-
 ///Edit Under This only
 
     var firebaseConfig = {
@@ -17,7 +13,3 @@ import { getDatabase } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-
 ////Do Not Edit Below This
 
 firebase.initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const database = getDatabase(app);
-
-console.log("Firebase Connected Successfully ✅");
