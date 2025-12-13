abe263b4b3a8ca9ddbe58fc8b627e37cb4feb6f3
moizrehman-creator <raeesmueez1@gmail.com>
2025-12-14 01:01:12 +0500

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ import { getDatabase } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-
 
     var firebaseConfig = {
     apiKey: "AIzaSyCH-8O3md6MKiE-vWSVSt6HiScVRgaviHc",
-    authDomain: "instagram-86afa.firebaseapp.com",
+    authDomain: "jade-tanuki-a948e4.netlify.app",
     databaseURL: "https://instagram-86afa-default-rtdb.firebaseio.com",
     projectId: "instagram-86afa",
     storageBucket: "instagram-86afa.firebasestorage.app",
