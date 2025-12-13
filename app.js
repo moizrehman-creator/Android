fd7b426d4be3c0fcee638c47ae2ca5bdb46cb560
moizrehman-creator <raeesmueez1@gmail.com>
2025-12-14 01:14:42 +0500

    Update Firebase configuration details

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 ///Edit Under This only
 
     var firebaseConfig = {
-    apiKey: "AIzaSyCH-8O3md6MKiE-vWSVSt6HiScVRgaviHc",
-    authDomain: "jade-tanuki-a948e4.netlify.app",
-    databaseURL: "https://instagram-86afa-default-rtdb.firebaseio.com",
-    projectId: "instagram-86afa",
-    storageBucket: "instagram-86afa.firebasestorage.app",
-    messagingSenderId: "575014280234",
-    appId: "1:575014280234:android:587327854fc7f6a2323753"
+  apiKey: "AIzaSyAqV2dqIKehmDN2K1x5yfGXPPo91LJckdI",
+  authDomain: "instagram-86afa.firebaseapp.com",
+  databaseURL: "https://instagram-86afa-default-rtdb.firebaseio.com",
+  projectId: "instagram-86afa",
+  storageBucket: "instagram-86afa.firebasestorage.app",
+  messagingSenderId: "575014280234",
+  appId: "1:575014280234:web:20929fb19bfd1ddc323753",
+  measurementId: "G-1ZG3MPTGG7"
   }; 
  
 ////Do Not Edit Below This
